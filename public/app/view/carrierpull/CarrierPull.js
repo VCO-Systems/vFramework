af2c28b247c1384f349c4507892eacd72af41aad
Vikrant Angia <vikrant.angia@vcosystems.com>
2014-08-17 00:39:45 -0400

    VFW-36 hook ShipVia dropdown to add and edit mode

--- public/app/view/carrierpull/CarrierPull.js
+++ public/app/view/carrierpull/CarrierPull.js
@@ -5,24 +5,6 @@
  *
  */
 
-var editModeColumns = [
-	{ text: 'Ship Via',  dataIndex: 'shipVia', header: 'Ship Via', sortable: false },
-	{ text: 'Ship Via Description',  dataIndex: 'todo', header: 'Ship Via Description', sortable: false  },
-	{ text: 'Pull Trailer Code',  dataIndex: 'pullTrlrCode', header: 'Pull Trailer Code',editor: {allowBlank: true}, sortable: false  },
-	{ text: 'Pull Time',  dataIndex: 'pullTime', header: 'pullTime', editor: {allowBlank: true}, sortable: false },
-	{ text: 'Pull Time AMPM',  dataIndex: 'pullTimeAMPM', header: 'Pull Time', editor: {allowBlank: true}, sortable: false },
-	{ text: 'Ship To Zip',  dataIndex: 'shipToZip',header: 'shiptoZip', sortable: false  }
-];
-
-var addModeColumns = [
-                   	{ text: 'Ship Via',  dataIndex: 'shipVia', header: 'shipVia',editor: {allowBlank: true}, sortable: false },
-                   	{ text: 'Ship Via Description',  dataIndex: 'todo', header: 'shipViaDescr', sortable: false  },
-                   	{ text: 'Pull Trailer Code',  dataIndex: 'pullTrlrCode', header: 'pullTrlrCode',editor: {allowBlank: true}, sortable: false  },
-                   	{ text: 'Pull Time',  dataIndex: 'pullTime', header: 'pullTime', editor: {allowBlank: true}, sortable: false },
-                   	{ text: 'Pull Time AMPM',  dataIndex: 'pullTimeAMPM', header: 'Pull Time AM/PM', editor: {allowBlank: true}, sortable: false },
-                   	{ text: 'Ship To Zip',  dataIndex: 'shipToZip',header: 'Ship To Zip', editor: {allowBlank: false}, sortable: false  }
-                   ];
-
 var rowEditing = Ext.create('Ext.grid.plugin.RowEditing', {
         clicksToMoveEditor: 2,
         autoCancel: true,
@@ -306,10 +288,19 @@ Ext.define('vfw.view.carrierpull.CarrierPull', {
                 	checkOnly: true,
                 	injectCheckbox: 0
                 },
-                
+                //fieldLabel: 'Ship Via'
                 columns: [
-                    { text: 'Ship Via',  dataIndex: 'shipVia', header: 'Ship Via', sortable: false,editor: {allowBlank: false} },
-                    { text: 'Ship Via Description',  dataIndex: 'todo', header: 'Ship Via Description', sortable: false  },
+                    { text: 'Ship Via',  dataIndex: 'shipVia', header: 'Ship Via', sortable: false,editor: {  name: 'RGHICarrierPull.Add.shipVia',
+																											  xtype: 'combobox',
+																											  queryMode: 'local', 
+																											  allowBlank: false,
+																											  forceSelection: true,
+																											  triggerAction: 'all',
+																											  maxWidth: 100,
+																											  displayField : 'shipVia',
+																											  valueField : 'shipVia',
+																											  store : 'ShipViaStore'} },
+                    { text: 'Ship Via Description',  dataIndex: 'todo', header: 'Ship Via Description', sortable: false, width: 300 },
                     { text: 'Pull Trailer Code',  dataIndex: 'pullTrlrCode', header: 'Pull Trailer Code',editor: {allowBlank: true}, sortable: false  },
                     { text: 'Pull Time',  dataIndex: 'pullTime', header: 'Pull Time', editor: {allowBlank: true}, sortable: false },
                     { text: 'Pull Time AMPM',  dataIndex: 'pullTimeAMPM', header: 'Pull Time AM/PM', editor: {allowBlank: true}, sortable: false },
